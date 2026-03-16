2cc7aba5181bcaca9709331744452ef296b80773
navinpatel76 <navinpatel7869@gmail.com>
2026-03-16 11:59:06 +0530

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://navinpatel76.github.io",
   base: "/NavinPatelPortfolio/",
   vite: {
     plugins: [tailwindcss()],
